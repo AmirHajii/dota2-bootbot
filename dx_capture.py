be08d3bdff399bec661016e318b6e14a242c7996
Dipster <mrtoxic.ir@gmail.com>
2026-07-24 00:06:08 +0330

    fix the bug for dx capture

--- dx_capture.py
+++ dx_capture.py
@@ -5,20 +5,17 @@ class DXCapture:
 
     def __init__(self, game_window):
         self.game_window = game_window
-
         self.camera = dxcam.create()
 
     def capture(self):
-
         region = (
             self.game_window.left,
             self.game_window.top,
             self.game_window.left + self.game_window.width,
             self.game_window.top + self.game_window.height
         )
-
-        frame = self.camera.grab(
-            region=region
-        )
-
-        return frame
+        try:
+            frame = self.camera.grab(region=region)
+            return frame
+        except ValueError:
+            return None
